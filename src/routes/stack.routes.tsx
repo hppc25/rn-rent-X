f3756d6636c68e7757bc6a5a2fe150275cc14e9d
Helder Correia <helder.patrick25@gmail.com>
2022-02-26 21:48:36 +0000

    add SignIn Screen

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -8,7 +8,7 @@ import { SchedulingDetails } from '../screens/SchedulingDetails';
 import { Confirmation } from '../screens/Confirmation';
 import { MyCars } from '../screens/MyCars';
 import { Splash } from '../screens/Splash';
-
+import { SignIn } from '../screens/SignIn';
 const { Navigator, Screen } = createStackNavigator();
 
 export function StackRoutes(){
@@ -18,8 +18,13 @@ export function StackRoutes(){
       screenOptions={{
         headerShown: false,
       }} 
-      initialRouteName="Splash"
+      initialRouteName="SignIn"
       >
+      <Screen 
+        name="SignIn"
+        component={SignIn}
+      />
+        
       <Screen 
         name="Splash"
         component={Splash}
